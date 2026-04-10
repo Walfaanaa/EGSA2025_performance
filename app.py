e24d60ef4ff68b63a4f16045e0a4fff7b308a7de
Walfaanaa Magarsaa Ejeta <119677853+Walfaanaa@users.noreply.github.com>
2026-04-10 17:23:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ df["score"] = (
     safe_div(df["loan_freq"], df["loan_freq"].max()) * 20 +
     safe_div(df["total_interest_amount"], df["total_interest_amount"].max()) * 15 +
     safe_div(df["monthly_payment"], df["monthly_payment"].max()) * 15 +
-    safe_div(df["Q2_achievement"], df["Q2_achievement"].max()) * 15 +
+    safe_div(df["achievement"], df["achievement"].max()) * 15 +
     safe_div(df["volentary_saving"], df["volentary_saving"].max()) * 10 +
     safe_div(df["fee_charge"], df["fee_charge"].max()) * 5 +
     safe_div(df["no_new_members_by"], df["no_new_members_by"].max()) * 20
